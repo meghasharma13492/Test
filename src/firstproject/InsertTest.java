2c5ccd610a83073c72c342344e8f951adf32c374
Megha Sharma <msharma@anpi.com>
2017-04-07 10:37:22 +0530

    Added OCR function to convert image to text

--- src/firstproject/InsertTest.java
+++ src/firstproject/InsertTest.java
@@ -1,31 +1,48 @@
 package firstproject;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 
-import java.awt.List;
-import java.util.ArrayList;
+import net.sourceforge.tess4j.Tesseract;
+import net.sourceforge.tess4j.TesseractException;
 
-import org.springframework.beans.factory.BeanFactory;  
-import org.springframework.beans.factory.xml.XmlBeanFactory;  
-import org.springframework.core.io.ClassPathResource;  
-import org.springframework.core.io.Resource;  
 
-import firstproject.beans.Activity;
-import firstproject.dao.ActivityDao;
-  
-public class InsertTest {  
-//public static void main(String[] args) {  
-//      
-//    Resource r=new ClassPathResource("applicationContext.xml");  
-//    BeanFactory factory=new XmlBeanFactory(r);  
-//      
-//    ActivityDao dao=(ActivityDao)factory.getBean("d");  
-//      
-//    ArrayList<Activity> e=new ArrayList<Activity>();  
-////    e.setId(114);  
-////    e.setName("varun"); 
-//    dao.getEmployees();
-////    e.setSalary(5000);  
-//      
-////    dao.saveEmployee(e);  
-//      
-//}  
-}  
+public class InsertTest {
+	public static void main(String[] args) {
+		
+		File imageFile = new File("eurotext.png");
+        Tesseract instance = Tesseract.getInstance(); // JNA Interface Mapping
+        instance.setDatapath("/usr/share/tesseract-ocr/tessdata");
+        // Tesseract1 instance = new Tesseract1(); // JNA Direct Mapping
+
+        try {
+            String result = instance.doOCR(imageFile);
+            System.out.println(result);
+           
+  	      try {
+  	    	File file = new File("/home/megha/Desktop/tmp.txt");
+			if (file.createNewFile()){
+			    System.out.println("File is created!");
+			  }else{
+			    System.out.println("File already exists.");
+			  }
+			FileWriter fw = new FileWriter(file.getAbsoluteFile());
+            BufferedWriter bw = new BufferedWriter(fw);
+
+            // Write in file
+            bw.write(result);
+
+            // Close connection
+            bw.close();
+	  	      } catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+        } catch (TesseractException e) {
+            System.err.println(e.getMessage());
+        }
+    
+		
+	}
+}
